30ae1ee3d962aff3a4e6c24449c9ce086dd734e4
Ashes987 <pavelnykn@gmail.com>
2022-01-14 22:19:48 +0300

    123

--- 123.py
+++ 123.py
@@ -4,7 +4,6 @@ spisok_slov = russian.read()
 poisk = spisok_slov.split('\n')
 import telebot
 bot = telebot.TeleBot('1925363878:AAGQfndPMvXk1Fe91rkuzp4l3ha9SwaWy74')
-import os
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
@@ -71,8 +70,7 @@ def get_text_messages(message):
 
     
 
-if __name__ == "__main__":
-    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+bot.polling(none_stop=True, interval=0)
 
 
 
